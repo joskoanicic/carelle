9873755a4cbe7303340da5559048fa2421b8b03a
vvetma <vedran.vetma@pinecone.hr>
2015-11-06 16:59:20 +0100

    Priprema za verziju 1.2

--- arelle/Version.py
+++ arelle/Version.py
@@ -5,4 +5,4 @@ This module represents the time stamp when Arelle was last built
 (c) Copyright 2015 Mark V Systems Limited, All rights reserved.
 
 '''
-version = '1.1 - 2015-03-06 14:48 UTC'
+version = '1.2 - 2015-11-05 16:55 UTC'
--- buildRenameX64.bat
+++ buildRenameX64.bat
@@ -1,1 +1,1 @@
-rename dist\cArelle-win-x64.exe cArelle-win-x64-1-1.exe
+rename dist\cArelle-win-x64.exe cArelle-win-x64-1-2.exe
